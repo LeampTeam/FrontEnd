59e1fa668f843e38888b09eaf2641399599c7f3c
RafaelZiraldo <rafazira83@gmail.com>
2020-04-28 10:00:10 -0300

    carro de compra

--- src/app/carro-compra/carro-compra-service.service.ts
+++ src/app/carro-compra/carro-compra-service.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { CarroCompraComponent } from './carro-compra.component';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: CarroCompraComponent
+  providedIn: 'root'
 })
 export class CarroCompraServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  obtenerArticulosShop(){
+    return this.http.get<any>('http://127.0.0.1:4000/shop/enviarArticulos')
+  }
 }
--- src/app/carro-compra/carro-compra.component.ts
+++ src/app/carro-compra/carro-compra.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/model/Usuario';
 
+import { CarroCompraServiceService } from './carro-compra-service.service';
+
 @Component({
   selector: 'app-carro-compra',
   templateUrl: './carro-compra.component.html',
   styleUrls: ['./carro-compra.component.css']
 })
 export class CarroCompraComponent implements OnInit {
-
+  public Articulos:Array<any>
   public usuario:Usuario
 
-  constructor() { }
+  constructor(private articuloService:CarroCompraServiceService) { }
 
   ngOnInit() {
+    this.articuloService.obtenerArticulosShop().subscribe(res =>{
+      this.Articulos=res;
+    })
   }
 
 }
